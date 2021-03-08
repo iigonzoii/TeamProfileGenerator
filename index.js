08cca81ce9008ea01cd3d370f31edad5d6f3c8dd
iigonzoii <a.gonzalez418@gmail.com>
2021-03-07 20:53:04 -0700

    added pseudo code

---
+++ index.js
@@ -0,0 +1,30 @@
+//!unit ten activity 28 mini project will help with the file structure
+// todo go over activities that go through testing like activity 15
+
+// im going to take input that i get through inquirer and im going to create cards with javascript that display the user inputed information 
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for my team members and their information
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
+
+// todo create switch statements for sure, functions that do stuff, write  a stupid test, im going to need a forloop somewhere so i can go through my array of objects to display my roles in cards
+
+//! this is going to be my reference for the inquirer prompts. first question should be what role to dictate data flow from there.
+//! include im done selecting roles as the last option of my question list for the select role object in inquirer
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated
