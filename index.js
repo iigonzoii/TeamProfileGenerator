d9a89fe936f5eda6c65e8f705eaf7844abf9e18f
iigonzoii <a.gonzalez418@gmail.com>
2021-03-09 13:58:49 -0700

    added rough draft of inquirer prompts

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
-//!unit ten activity 28 mini project will help with the file structure
-// todo go over activities that go through testing like activity 15
+const inquirer = require(`inquirer`)
+const fs = require(`fs`)
+const util = require('util')
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
 
-// im going to take input that i get through inquirer and im going to create cards with javascript that display the user inputed information 
+// create writeFile function using promises instead of a callback function(util.promisfy)
+// writeFile instead of write so it keeps trying until all of data is written or an error occurs
+const writeFileAsync = util.promisify(fs.writeFile)
 
+// todo create main question prompt
+// const promptUser = async () => { inquirer.prompt([..... is that how i would write an async arrow function? do i need the curly after the arrow?
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email address?",
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager"
+            ]
+        },
+    ]);
 
+    // todo create prompt for Engineer
+    //  does it matter where these commas go? punctuation/syntax may be wrong!
+await inquirer.prompt([{
+    type: "input",
+    name: "?",
+    message: "What is the employee's github username?",
+}
+])
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-
-// todo create switch statements for sure, functions that do stuff, write  a stupid test, im going to need a forloop somewhere so i can go through my array of objects to display my roles in cards
-
-//! this is going to be my reference for the inquirer prompts. first question should be what role to dictate data flow from there.
-//! include im done selecting roles as the last option of my question list for the select role object in inquirer
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+    // todo create prompt for Intern
+    //  does it matter where these commas go? punctuation/syntax may be wrong!
+    await inquirer.prompt([{
+        type: "input",
+        name: "?",
+        message: "What is the employee's github username?",
+    }
+    ])  
